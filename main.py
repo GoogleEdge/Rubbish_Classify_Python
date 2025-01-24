f7f50bc825f9b459551fd9314dfd290aec49ac6e
Dev_GoogleEdge <137889657+GoogleEdge@users.noreply.github.com>
2025-01-24 17:22:50 +0800

    添加上传按钮以选择多张图片并进行处理

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import tkinter
 from tkinter import filedialog
 
 root = tkinter.Tk()
-root.withdraw()
 
-img_path = filedialog.askopenfilename(title='选择你需要识别的图片')
-if img_path:
-    aiphoto(img_path)
-else:
-    print('未选择图片')
+upload_button = tkinter.Button(root, text='上传图片', command=lambda: upload())
+upload_button.pack()
+
+def upload():
+    img_path_tuple = filedialog.askopenfilenames(title='选择你需要识别的图片')
+    img_path = str(img_path_tuple[0])
+    if img_path:
+        aiphoto(img_path)
+    else:
+        print('未选择图片')
 root.mainloop()
