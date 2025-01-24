e1e1566c21748dd0567f7001f88de4b9f204fecf
Dev_GoogleEdge <137889657+GoogleEdge@users.noreply.github.com>
2025-01-24 17:22:23 +0800

    修复了无法获取文件路径的问题，提高了稳定性

--- aiphoto.py
+++ aiphoto.py
@@ -5,7 +5,8 @@ def aiphoto(img_path):
     with open(img_path, 'rb') as img_file:
         img_base = base64.b64encode(img_file.read()).decode('utf-8')
     key = input("请输入你的key：")
-    client = ZhipuAI(api_key=key)
+    if key:
+      client = ZhipuAI(api_key=key)
     response = client.chat.completions.create(
     model="glm-4v-flash",
     messages=[
@@ -20,7 +21,7 @@ def aiphoto(img_path):
           },
           {
             "type": "text",
-            "text": "直接告诉这个图片中的东西垃圾分类在哪里，不要输出其他内容(例如：可回收、不可回收、有害、厨余)"
+            "text": "直接输出这个图片中的东西应该分类在哪种垃圾，不要输出其他内容。格式：厨余垃圾/其他垃圾/可回收垃圾/有害垃圾"
           }
         ]
       }
